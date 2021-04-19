40f6b134e7ced6794d78e5a0a2c136c39459b93c
Juan Carlos Pedrique <jpedrique@controlps.com>
2021-04-19 01:12:45 -0400

    Update prueba.sh
    Se agregaron colores a los subtitulos

--- prueba.sh
+++ prueba.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # PRACTICA DE USO DE SCRIPT
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+#printf "I ${RED}love${NC} Stack Overflow\n"
 
-echo "PRUEBAS DE EJECUCION DE COMANDO LINUX"
-apt install figlet -y
+printf "${GREEN}PRUEBAS DE EJECUCION DE COMANDO LINUX${NC}\n"
+apt install figlet -y >/dev/null 2>&1
 figlet "Lab k8s"
 
-echo "[TAREA 1] Crear directorio y archivo"
-sudo su
-mkdir practica
+printf "${GREEN}[TAREA 1] CREAR DIRECTORIO Y ARCHIVO${NC}\n"
+mkdir -p practica
 cd practica
-touch archivo.txt
-echo "Solo es una prueba" > archivo.txt
-cat archivo.txt
+touch archivo1.txt
+echo "Solo es una prueba" > archivo1.txt
+cat archivo1.txt
 
-echo "[TAREA 2] Mostrar IP, nombrede Host"
+printf "${GREEN}[TAREA 2] MOSTRAR IP Y NOMBRE DEL HOST${NC}\n"
 ip a|grep inet
 hostname
 
-echo "[TAREA 3] Mostrar memoria y capacidad de disco"
+printf "${GREEN}[TAREA 3] MOSTRAR MEMORIA Y CAPACIDAD DE DISCO${NC}\n"
 free -h
-df -h |grep dev
-echo "[TAREA 3] Fin del scrip"
+df -h |grep dev/root
+printf "${GREEN}[TAREA 3] Fin del scrip${NC}\n"
