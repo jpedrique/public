12d2dfe8db78d4e853fdf68f7c1b430b1bb07674
Juan Carlos Pedrique <jpedrique@controlps.com>
2021-04-19 09:07:11 -0400

    Update joincluster.sh
    Solo se le agregó sudo al comando de kubeadm

--- joincluster.sh
+++ joincluster.sh
@@ -33,4 +33,4 @@ echo "export TERM=xterm" >> /etc/bash.bashrc
 cat >>/etc/hosts<<EOF
 172.31.33.110	kmaster
 EOF
-kubeadm join 172.31.33.110:6443 --token bqn9xp.szr0ddfv6jc5nv1n     --discovery-token-ca-cert-hash sha256:bc731b713b99736d81c2984c550695e571027af9eb9556790d0784ae7e575c59
+sudo kubeadm join 172.31.33.110:6443 --token bqn9xp.szr0ddfv6jc5nv1n     --discovery-token-ca-cert-hash sha256:bc731b713b99736d81c2984c550695e571027af9eb9556790d0784ae7e575c59
