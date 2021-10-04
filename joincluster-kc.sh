985fd7f18e98fd45e21646f0d49a3312bed1be8c
Juan Carlos Pedrique <jpedrique@controlps.com>
2021-10-04 12:37:22 -0400

    Update joincluster-kc.sh
    Se agragaron IP + Nombre de PC fijos en el /etc/hosts

--- joincluster-kc.sh
+++ joincluster-kc.sh
@@ -29,12 +29,14 @@ echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
 echo "export TERM=xterm" >> /etc/bash.bashrc
-#cat >>/etc/hosts<<EOF
-#172.31.33.110	kmaster
-#EOF
 hostnamectl set-hostname kanban-w01
 hostname -i | cut -c1-13
 IPA=$(hostname -i | cut -c1-13)
-
-echo -e "${IPA}\t${HOSTNAME}" >> /etc/hosts
+#echo -e "${IPA}\t${HOSTNAME}" >> /etc/hosts
+cat >>/etc/hosts<<EOF
+172.31.34.201 kanban-m01
+172.31.34.202 kanban-m02
+172.31.34.101 kanban-w01
+172.31.34.102 kanban-w02
+EOF
 
